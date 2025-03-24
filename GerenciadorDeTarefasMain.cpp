a542674fae52635799b4c2d68914324be71c907e
NaelNunes <107895361+NaelNunes@users.noreply.github.com>
2025-03-24 10:51:44 -0300

    Add files via upload

---
+++ GerenciadorDeTarefasMain.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <windows.h>
+#include <ctype.h>
+#include <string.h>
+#include "TADGerenciadorDeTarefas.h"
+
+void InterfaceMenu(void)
+{
+	
+}
+
+void RelatorioLoop(void)
+{
+	
+}
+
+int main()
+{
+	
+	return 0;
+}
---
+++ TADGerenciadorDeTarefas.h
@@ -0,0 +1,57 @@
+#define MAXFILA 100
+
+struct TpTarefas
+{
+	int tipo; // 1, 2 ou 3 para o nivel de prioridade
+	int tempoConc; // Tempo de conclusão estimado
+	char nomeTarefa[40]; // Nome da tarefa
+	char devResp[20]; // Desenvolvedor Responsável
+	char dataIni[9]; // Data do inicio
+}
+
+struct TpFila
+{
+	int FIM;
+	TpTarefas FILAP[MAXFILA];
+}
+
+void Inicializar(TpFila &F);
+
+void Inicializar(TpFila &F)
+{
+	F.FIM = -1;
+}
+
+void Inserir(TpFila &FP, TpElemento Elem)
+{
+	FP.FILAP[++FP.FIM] = Elem;
+	int i = F.FIM;
+	
+	while(i > 0 && FP.FILAP[i-1].Prioridade > FP.FILAP[i].Prioridade)
+	{
+		FP.FILAP[i] = FP.FILAP[i - 1];
+		FP.FILAP[i - 1] = Elem;
+		i--;
+	}
+}
+
+char Vazia(int FIM)
+{
+	return FIM == -1;
+}
+
+char Cheia(int FIM)
+{
+	return FIM == MAXFILA - 1;
+}
+
+TpElemento Retirar(TpFila &FP)
+{
+	return FP.FILAP[0];
+}
+
+void Exibir(TpFila FP)
+{
+	while(!Vazia(FP.FIM))
+		printf("\t%s",Retirar(FP));
+}
