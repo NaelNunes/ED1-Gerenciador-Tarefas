940cc1840c9c92d15ccb83f7d0998f7b8784a6f2
NaelNunes <107895361+NaelNunes@users.noreply.github.com>
2025-03-24 20:35:57 -0300

    Add files via upload

--- GerenciadorDeTarefasMain.cpp
+++ GerenciadorDeTarefasMain.cpp
@@ -15,8 +15,41 @@ void RelatorioLoop(void)
 	
 }
 
+void Simulacao(TpFila &RegFila, TpSimulacao &RegSim, FILE *Ptr)
+{
+	Inserir(RegFila,LerArquivo(Ptr));
+	
+}
+
 int main()
 {
+	do
+	{
+		TpFila RegFila;
+		TpSimulacao RegSim;
+	
+		FILE *Ptr = AbreArquivo();
+		int limiteDev;
+		int duracaoTempo;
+		
+		printf("Digite o numero de devs: ");
+		scanf("%d", &limiteDev);
+		printf("Digite o tempo maximo de simulacao: ");
+		scanf("%d", &duracaoTempo);
+		 
+		while(!Cheia(RegFila.FIM) && RegSim.devs < limiteDev && RegSim.tempo < duracaoTempo)
+		{
+			
+		}	
+			
+		
+		
+		
+		printf("Deseja realizar outra simulacao? (S/N)");
+	} while(toupper(getch()) == 'S');
+	
+		
+	
 	
 	return 0;
 }
--- TADGerenciadorDeTarefas.h
+++ TADGerenciadorDeTarefas.h
@@ -2,27 +2,56 @@
 
 struct TpTarefas
 {
-	int tipo; // 1, 2 ou 3 para o nivel de prioridade
-	int tempoConc; // Tempo de conclusão estimado
+	char tipo[12]; // 1, 2 ou 3 para o nivel de prioridade
+	int tempoConc; // Tempo de conclusao estimado
 	char nomeTarefa[40]; // Nome da tarefa
-	char devResp[20]; // Desenvolvedor Responsável
-	char dataIni[9]; // Data do inicio
-}
+	char devResp[20]; // Desenvolvedor Responsavel
+	char dataIni[11]; // Data do inicio
+};
 
 struct TpFila
 {
 	int FIM;
 	TpTarefas FILAP[MAXFILA];
-}
+};
 
+struct TpSimulacao
+{
+	int devs = 0;
+	int tempo = 0;
+};
+
+struct TpRelatorio
+{
+	int tarefasConc;
+	float tempoMedio;
+	int tarefasIncomp;
+};
+
+// Escopo para fila
 void Inicializar(TpFila &F);
+void Inserir(TpFila &FP, TpElemento Elem);
+char Vazia(int FIM);
+char Cheia(int FIM);
+TpTarefas Retirar(TpFila &FP);
+void Exibir(TpFila FP);
+//
 
+// Escopo para arquivo
+FILE* AbreArquivo(void);
+TpTarefas LerArquivo(FILE *Ptr);
+//
+
+
+
+
+// Funcoes para fila
 void Inicializar(TpFila &F)
 {
 	F.FIM = -1;
 }
 
-void Inserir(TpFila &FP, TpElemento Elem)
+void Inserir(TpFila &FP, TpTarefas Elem)
 {
 	FP.FILAP[++FP.FIM] = Elem;
 	int i = F.FIM;
@@ -64,3 +93,29 @@ void Exibir(TpFila FP)
 	while(!Vazia(FP.FIM))
 		printf("\t%s",Retirar(FP));
 }
+//
+
+
+
+
+// Funcoes para arquivo
+
+FILE* AbreArquivo(void)
+{
+	FILE *Ptr = fopen("Arquivo.txt","r");
+	return Ptr;
+}
+
+TpTarefas LerArquivo(FILE *Ptr)
+{
+	TpTarefas Reg;
+	
+	fscanf(Ptr,"%[^,],%d,%[^,],%[^,],%s\n", &Reg.tipo, &Reg.tempoConc, &Reg.nomeTarefa, &Reg.devResp, &Reg.dataIni);
+	return Reg;
+}
+//
+
+
+
+
+
